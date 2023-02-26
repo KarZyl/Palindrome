13277e1a612fb97c4bc3fc250ecae69e4b529c00
KarZyl <125766453+KarZyl@users.noreply.github.com>
2023-02-26 11:35:51 +0100

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,22 +1,11 @@
-
 def palindrome_check(word):
     """
        Checks if given word is a palindrome
     """
-    position_half_word = int((len(word)-len(word) % 2) / 2)
-        
-    for a in range(0,position_half_word):
-      if word[a] == word[-(a+1)]:
-        pass
-        check = ""
-      else:
-        check = " not"        
-        break
-    print (f"Given word is{check} a palindrome")
+    check = (word == word[::-1])
+    result = (f"Given word is a palindrome: {check}")
+    return result
     
-      
-    
-  
 
 
-
+print(palindrome_check("kajak"))
