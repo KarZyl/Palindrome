7d05f5850d67450394038ae09c9944d2ffce7a05
KarZyl <125766453+KarZyl@users.noreply.github.com>
2023-02-26 13:02:59 +0100

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -2,9 +2,8 @@ def palindrome_check(word):
     """
        Checks if given word is a palindrome
     """
-    check = (word == word[::-1])
-    result = (f"Given word is a palindrome: {check}")
-    return result
+    check = word == word[::-1]
+    return check
     
 
 
